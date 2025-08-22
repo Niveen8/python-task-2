801640851e1409df47219fe4c02c3027702e499f
Niveen <niveenabutamam@gmail.com>
2025-08-23 01:56:32 +0300

    print

--- taketow.py
+++ taketow.py
@@ -4,4 +4,5 @@ password = input("Enter your  password: ")
 username = username.lower()
 username = username.replace(" ", "_")
 password_length = len(password) #فينا نعملها عن طريق for loop
-
+print("Name:"+username)
+print("Password:",password)
