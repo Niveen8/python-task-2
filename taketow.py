1330676fa5ff52fa707678b3bea2779528e9f70e
Niveen <niveenabutamam@gmail.com>
2025-08-23 02:09:29 +0300

    cheack pass and put derult

--- taketow.py
+++ taketow.py
@@ -8,3 +8,8 @@ print("Name:"+username)
 print("Password:",password)
 print("If the password length is greater than or equal to 8:", password_length >= 8)
 print("If the username is admin:", username == "admin")
+print("If the password is 1234:", password == "1234")
+print("Check if it's default account → (username is \"admin\" and the password is \"1234\"):", username == "admin" and password == "1234")
+
+
+
