44385781aace5358ccbb560522a24091357579f8
Niveen <niveenabutamam@gmail.com>
2025-08-23 02:06:00 +0300

    check name

--- taketow.py
+++ taketow.py
@@ -7,3 +7,4 @@ password_length = len(password) #فينا نعملها عن طريق for loop
 print("Name:"+username)
 print("Password:",password)
 print("If the password length is greater than or equal to 8:", password_length >= 8)
+print("If the username is admin:", username == "admin")
